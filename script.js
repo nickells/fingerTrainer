6945cc82a82f05a5cf3d2cf1a5e24b83d201b18f
nickells <ellsworth.nick@gmail.com>
2016-04-29 21:41:42 -0400

    added featires

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 //todo
-//iterate with weighting
 //streak
-//dont use dom as data source
 
 var gameObj = {
   keyIn: document.getElementById('keyboardInput'),
   feedback: document.getElementById('feedback'),
-  letter: document.getElementById('typeThis'),
+  letterDOM: document.getElementById('typeThis'),
+  streakDOM: document.getElementById('streak'),
+  letter: '',
   selections: ['!','@','#','$','%','^','&','*','(',')','_'],
+  // selections: ['&'],
   getRandomSelection: function(){
     return this.selections[Math.floor(Math.random()*this.selections.length)];
   },
   setRandomKey: function(){
-    this.letter.innerHTML = this.getRandomSelection()
+    this.letter = this.getRandomSelection();
+    this.letterDOM.innerHTML = this.letter;
   },
   renderErrors: function(){
     var html = 'Errors: \n'
@@ -21,24 +23,34 @@ var gameObj = {
     }
     this.feedback.innerHTML = html;
   },
+  renderStreak: function(){
+    var html = 'Streak: '
+    html += this.streak;
+    this.streakDOM.innerHTML = html;
+  },
   currentPercent: 100,
-  mistypes: {}
+  mistypes: {},
+  streak: 0
 }
 
 gameObj.keyIn.focus()
-gameObj.setRandomKey()
+gameObj.setRandomKey();
 
 gameObj.keyIn.addEventListener('keyup',function(e){
   var userInput = e.target.value;
-  var correctKey = this.letter.innerHTML;
+  var correctKey = this.letter;
   if (userInput === '') return;
-  if (userInput === '&') userInput = '&amp;';
   if (correctKey === userInput){
     this.setRandomKey();
+    this.streak++;
+    this.renderStreak();
   }
   else {
     this.mistypes[correctKey] ? this.mistypes[correctKey]++ : this.mistypes[correctKey] = 1;
+    this.selections.push(correctKey); //weighting!
     this.renderErrors();
+    this.streak = 0;
+    this.renderStreak();
   }
   this.keyIn.value = '';
 }.bind(gameObj))
