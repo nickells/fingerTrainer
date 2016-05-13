29edcc75e63ca4f3f808a6bcc0f647d5939663a9
nickells <ellsworth.nick@gmail.com>
2016-05-13 00:29:42 -0400

    eslinted

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
-//todo: simulate hold shift
+// todo: simulate hold shift
 
-var gameObj = {
+const gameObj = {
   keyIn: document.getElementById('keyboardInput'),
   feedback: document.getElementById('feedback'),
   letterDOM: document.getElementById('typeThis'),
   streakDOM: document.getElementById('streak'),
   letter: '',
-  selections: ['!','@','#','$','%','^','&','*','(',')','_'],
-  getRandomSelection: function(){
-    return this.selections[Math.floor(Math.random()*this.selections.length)];
+  selections: ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_'],
+  getRandomSelection() {
+    return this.selections[Math.floor(Math.random() * this.selections.length)];
   },
-  setRandomKey: function(){
+  setRandomKey() {
     this.letter = this.getRandomSelection();
     this.letterDOM.innerHTML = this.letter;
   },
-  renderErrors: function(){
-    var html = 'Errors: \n'
+  renderErrors() {
+    let html = 'Errors: \n';
     for (var key in this.mistypes){
-      html+='<div>'+key+': ' + this.mistypes[key] + '</div>'
+      html += '<div>' + key + ': ' + this.mistypes[key] + '</div>';
     }
     this.feedback.innerHTML = html;
   },
-  renderStreak: function(){
-    var html = 'Streak: '
+  renderStreak() {
+    let html = 'Streak: ';
     html += this.streak;
     this.streakDOM.innerHTML = html;
   },
   currentPercent: 100,
   mistypes: {},
-  streak: 0
-}
+  streak: 0,
+};
 
-gameObj.keyIn.focus()
+gameObj.keyIn.focus();
 gameObj.setRandomKey();
 
-gameObj.keyIn.addEventListener('keyup',function(e){
-  var userInput = e.target.value;
-  var correctKey = this.letter;
+gameObj.keyIn.addEventListener('keyup', function keyUpListener(e) {
+  const userInput = e.target.value;
+  const correctKey = this.letter;
   if (userInput === '') return;
-  if (correctKey === userInput){
+  if (correctKey === userInput) {
     this.setRandomKey();
     this.streak++;
     this.renderStreak();
-    this.keyIn.placeholder = 'nice job!'
-  }
-  else {
+    this.keyIn.placeholder = 'nice job!';
+  } else {
     this.mistypes[correctKey] ? this.mistypes[correctKey]++ : this.mistypes[correctKey] = 1;
-    this.selections.push(correctKey); //weighting!
+    this.selections.push(correctKey); // weighting!
     this.renderErrors();
     this.streak = 0;
-    this.keyIn.placeholder = 'try again!'
+    this.keyIn.placeholder = 'try again!';
     this.renderStreak();
   }
   this.keyIn.value = '';
-}.bind(gameObj))
+}.bind(gameObj));
